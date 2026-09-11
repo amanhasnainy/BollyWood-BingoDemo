b45f6ed3add0763dc3cd4b875184a02d33ade020
Aman hasnainy <hasnainyaman@gmail.com>
2026-09-11 15:06:59 +0530

    fixe

--- client/src/global/api.ts
+++ client/src/global/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://different.desicomedylive.com/";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://different.desicomedylive.com";
 
 export function buildApiUrl(path: string) {
   if (!API_BASE_URL) {
